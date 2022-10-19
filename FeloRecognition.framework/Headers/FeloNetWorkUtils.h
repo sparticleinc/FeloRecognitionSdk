8465f5ed9e46ef6445109ad26111e24cad119047
lee.zhu <lee.zhu@sparticle.com>
2022-10-19 11:28:21 +0800

    sdk v1.1

--- FeloRecognition.framework/Headers/FeloNetWorkUtils.h
+++ FeloRecognition.framework/Headers/FeloNetWorkUtils.h
@@ -6,8 +6,17 @@
 //
 
 #import <Foundation/Foundation.h>
+
+//@protocol FeloNetWorkUtilsDelegate<NSObject>
+//@required
+//- (void)tokenIsExpired:(BOOL)isExpired;
+//@end
+
 @interface FeloNetWorkUtils : NSObject
 +(instancetype)manager;
+
+//@property(weak,nonatomic)id<FeloNetWorkUtilsDelegate> delegate;
+
 @property(strong,nonatomic) NSString *appId;
 @property(strong,nonatomic) NSString *authToken;
 - (void)feloPost:(NSString *)url  parameters:(NSMutableDictionary *)parameters header:(NSString *)token successed:(void (^)(id responseObject))successed failed:(void (^)(NSError *error))failed;
--- FeloRecognition.framework/Headers/SpeechRecognModel.h
+++ FeloRecognition.framework/Headers/SpeechRecognModel.h
@@ -7,57 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+typedef void(^RfreshTokenBlock)(NSString *) ;
+typedef void (^RreshAccessTokenBlock)(BOOL expired);
 @protocol FeloSubtitleDelegate <NSObject>
 //Here are some things you can do when the network goes down
 - (void)disconnectServer;
 //We will automatically reconnect the subtitle service when the network is restored
 - (void)reconnectedServer;
-
 @required
 - (void)getSubtitle:(NSDictionary *)subTitle;
+/// call when refresh token is expired
+/// - Parameter refreshBlock: refreshBlock description
+//- (void)refreshToken:(void(^)(NSString *))refreshBlock;
 
 @end
 
 @interface SpeechRecognModel : NSObject
 + (SpeechRecognModel *)sharedObject;
 
-@property (assign,nonatomic) BOOL autoConnect;//It is recommended to open it so that the subtitle sdk can be automatically connected when the network is disconnected
-@property (weak,nonatomic) id<FeloSubtitleDelegate> delegate;
+//It is recommended to open it so that the subtitle sdk can be automatically connected when the network is disconnected
+@property (assign,nonatomic) BOOL autoConnect;
 
+@property (copy,nonatomic)RreshAccessTokenBlock refreshBlock;
+@property (weak,nonatomic) id<FeloSubtitleDelegate> delegate;
+@property (strong,nonatomic) NSString *authToken;
 /// start subtitle
 /// - Parameters:
 ///   - params: params contian appid,accesstoken
 ///   - completeHandler: completeHandler description
 - (void)startSubtitle:(NSMutableDictionary *)params completeHandler:(void (^)(BOOL))completeHandler;
-/// GET MQTT INFO
-/// @param clientType WEB、DESKTOP、IOS、ANDROID
-/// @param completeHandler completeHandler description
-//- (void)getMqttInfo:(NSString *)clientType completeHandler:(void (^)(NSDictionary *))completeHandler;
-
-/// get auth token for userid
-/// @param userID userID description
-//- (void)getAuthToken:(NSString *)userID completeHandler:(void(^)(NSDictionary *))completeHandler;
-
-//- (void)refreshToken:(NSString *)grant_type  completeHandler:(void (^)(NSDictionary *))completeHandler;
-
-/// Description
-/// @param bizId bizId description
-/// @param language language description
-/// @param completeHandler completeHandler description
-//- (void)createConversation:(NSString *)bizId language:(NSString *)language completeHandler:(void (^)(BOOL ))completeHandler;
-
-/// get conversation
-/// @param bizId <#bizId description#>
-/// @param completeHandler <#completeHandler description#>
-//- (void)getConversation:(NSString *)bizId  completeHandler:(void (^)(NSDictionary *))completeHandler;
-
-//- (void)joinConversation:(NSString *)bizId  completeHandler:(void (^)(NSDictionary *))completeHandler;
 
 /// leave room
 /// - Parameters:
-///   - bizId: bizid
 ///   - completeHandler: completeHandler description
-- (void)leaveConversation:(NSString *)bizId  completeHandler:(void (^)(BOOL flag))completeHandler;
+- (void)leaveConversation:(void (^)(BOOL flag))completeHandler;
 
 /// set room language
 /// - Parameters:
